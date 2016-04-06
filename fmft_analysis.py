bf3d4e86582886ac3a9d8255f70884f80e033ebb
Arthur Bulin <arthurbulin@gmail.com>
2016-04-06 02:09:06 -0500

    Flushed out routine. Now operational.

--- fmft_analysis.py
+++ fmft_analysis.py
@@ -1,20 +1,26 @@
 import os
 import lib_merc as lm
+import numpy as np
 
 absw = '/home/bulin/Mercury/'
 
-def fmft_analysis(lookin):
+def main(lookin):
 	for i in xrange(len(lookin)):
+		delF,delfs,delf_dats = list(),dict(),dict()
+		worlds = ['MERCURY','MARS']
 		print 'For ' + lookin[i]
-		folders = lm.get_dir(lookin[i])
+		folders = lm.lib_get_dir(lookin[i])
 		folders.sort()
-		for j in xrange(len(folders)):
-			ifiles,ffiles = list(),list()
-			
-			dir_con = os.listdir(absw + lookin[i] +'/'+folders[j])
-			
-			for h in dir_con:
-				if '1.fmft' in h: ifiles.append(h)
-				if '2.fmft' in h: ffiles.append(h)
-			for k in xrange(len(ifiles)):
-				F,A,P = np.genfromtxt(ifiles[k],unpack=True)
+		for w in worlds:
+			for j in xrange(len(folders)):
+				ifiles,ffiles = list(),list()
+				dir_con = os.listdir(absw + lookin[i] +'/'+folders[j])
+				for h in dir_con:
+					if w+'1.fmft' in h: first = h
+					if w+'2.fmft' in h: sec = h
+				Fi,Ai,Pi = np.genfromtxt(absw + lookin[i] +'/'+ folders[j] +'/'+ first,unpack=True)
+				Ff,Af,Pf = np.genfromtxt(absw + lookin[i] +'/'+ folders[j] +'/'+ sec,unpack=True)
+				delF.append(Ff[0] - Fi[0])
+			delfs[w] = delF
+		delf_dats[lookin[i]] = delfs
+	return delf_dats
