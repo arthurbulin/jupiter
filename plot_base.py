7e81ef4eac1bf5f45836ab01e2ba0edc12846bd7
Arthur Bulin <arthurbulin@gmail.com>
2016-04-06 10:07:15 -0500

    Added type=2 for histograms.

--- plot_base.py
+++ plot_base.py
@@ -6,7 +6,7 @@ from matplotlib.ticker import AutoMinorLocator
 import astropy.constants as const
 
 #x1 and d are list type, or nparray type
-def plot_base(x1,d,dolegend=False,names=None,yaxis_title=None,xaxis_title=None,title=None,x_limit=None,y_limit=None,set_dpi=128):
+def plot_base(x1,d,type=1,dolegend=False,names=None,yaxis_title=None,xaxis_title=None,title=None,x_limit=None,y_limit=None,set_dpi=128):
 
 	minorLocator = AutoMinorLocator()
 
@@ -19,14 +19,17 @@ def plot_base(x1,d,dolegend=False,names=None,yaxis_title=None,xaxis_title=None,t
 
 	#Iterate through for each array in the list d and add it to the plot
 #		ax1.plot(x1,d[i],linestyle=linestyles[i],label=labels[i],lw=2)
-	if names is not None:
-		for i in xrange(len(d)):
-			ax1.plot(x1,d[i],label=names[i],lw=2)
-	else:
-		for i in xrange(len(d)):
-			ax1.plot(x1,d[i],lw=2)
 
-
+	if type == 1:
+		if names is not None:
+			for i in xrange(len(d)):
+				ax1.plot(x1,d[i],label=names[i],lw=2)
+		else:
+			for i in xrange(len(d)):
+				ax1.plot(x1,d[i],lw=2)
+	if type == 2:
+		for i in xrange(len(d)):
+			ax1.hist(d[i])
 
 	#Assign Axis labels, title, and ticks
 	if yaxis_title is not None: ax1.set_ylabel(yaxis_title,fontsize=18)
