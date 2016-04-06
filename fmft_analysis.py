951c1a1196112edfc90f12ab2db1885540e4373f
Arthur Bulin <arthurbulin@gmail.com>
2016-04-06 10:06:51 -0500

    Corrected for wrong return.

--- fmft_analysis.py
+++ fmft_analysis.py
@@ -5,19 +5,22 @@ import numpy as np
 absw = '/home/bulin/Mercury/'
 
 def main(lookin):
+	delf_dats = dict()
 	for i in xrange(len(lookin)):
-		delF,delfs,delf_dats = list(),dict(),dict()
+		delfs = dict()
 		worlds = ['MERCURY','MARS']
 		print 'For ' + lookin[i]
 		folders = lm.lib_get_dir(lookin[i])
 		folders.sort()
 		for w in worlds:
+			delF = list()
 			for j in xrange(len(folders)):
 				ifiles,ffiles = list(),list()
 				dir_con = os.listdir(absw + lookin[i] +'/'+folders[j])
 				for h in dir_con:
 					if w+'1.fmft' in h: first = h
 					if w+'2.fmft' in h: sec = h
+				print first,sec
 				Fi,Ai,Pi = np.genfromtxt(absw + lookin[i] +'/'+ folders[j] +'/'+ first,unpack=True)
 				Ff,Af,Pf = np.genfromtxt(absw + lookin[i] +'/'+ folders[j] +'/'+ sec,unpack=True)
 				delF.append(Ff[0] - Fi[0])
