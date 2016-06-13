7ef7d6072289fffc2b37920273b3af6cd3255cb0
Arthur Bulin <arthurbulin@gmail.com>
2016-06-13 18:22:04 -0500

    Added export to csv for sims

--- fmft_analysis.py
+++ fmft_analysis.py
@@ -21,7 +21,7 @@ def main(lookin,worlds):
 		print 'For ' + lookin[i]
 		
 		if do_hdf5 is 'true':
-			folders = h5b.get_keys(lookin[i])
+			folders = h5b.get_keys(sim=lookin[i])
 		else:
 			folders = lme.lib_get_dir(lookin[i])
 		
@@ -53,3 +53,85 @@ def plot_hist(dat,worlds,look,save=False):
 			else:
 				pb.plot_base(None,[to_plot],type=2,title=r'Histogram of |$\Delta f$| for '+w+' for Sim '+look[i],xaxis_title=r'|$\Delta f$|',yaxis_title='Number of occurances')
 			
+def collect_fmft(sim,mode=0):
+#	os.chdir(data_out+sim)
+	dir = data_out+sim+'/'
+	dir_list = os.listdir(dir)
+	dir_list.sort()
+	
+	dirs = list() #For the good directories
+	for i in dir_list:
+		#Check for my runs in int named folders
+		try: 
+			int(i)
+			num = True
+		except:
+			num = False
+			
+		if (os.path.isdir(dir+i)) and (num == True):
+			dirs.append(i)
+	dirs.sort()
+	
+	for i in dirs:
+		data,names,fmft_l = list(),list(),list()
+		printlines,planets = list(),list()
+		set_dir = os.listdir(dir+i)
+#		print set_dir
+		
+		for j in set_dir:
+			if str(mode)+'.fmft' in j:
+				fmft_l.append(j)
+#		print fmft_l
+		for j in fmft_l:
+			value,mag,phase = np.genfromtxt(dir+i+'/'+j,unpack=True)
+			names.append(j.split(str(mode))[0])
+			data.append(value)
+#		names.sort()
+		
+		#TOO MANY DAMN LOOPS, but i'm exhausted and i just want it in order right now. ill fix later
+#		not_in,names_in = list(),list()
+#		names_default = ['mercury','venus','earth','mars','jupiter','saturn']
+#		for j in names_default:
+#			if str.upper(j) in names:
+#				names_in.append(j)
+#		print 'names'
+#		print names_in
+#		for j in names:
+#			if str.lower(j) not in names_default:
+#				not_in.append(j)
+	
+	##	for j in not_in:		
+	#		names_in.append(j)
+		
+#		if len(not_in) > 0:
+#			for j in not_in:
+#				names_in.append(j)
+#		print names_in
+#		for j in names_in:
+##			try: value,mag,phase = np.genfromtxt(dir+i+'/'+j+str(mode)+'.fmft',unpack=True)
+#			except IOError:
+#				try: value,mag,phase = np.genfromtxt(dir+i+'/'+str.upper(j)+str(mode)+'.fmft',unpack=True)
+#				except:
+#					print "Something odd."
+#					return 66
+#			data.append(value)
+#			
+		dat = np.asarray(data)
+		dat = np.fliplr(np.flipud(np.rot90(dat)))
+		for j in dat:
+			tmp = ''
+			for k in j:
+				tmp = tmp+','+str(k)
+			printlines.append(tmp.split(',',1)[1])
+		
+		tmp = ''
+		names.reverse()
+		for j in names:
+			tmp = tmp +','+str(j)
+		namesprint = tmp.split(',',1)[1]
+		
+		f = open(dir+i+'/0_worlds_fmft.csv','w')
+		f.write(namesprint+'\n')
+		for j in printlines:
+			f.write(j+'\n')
+		f.close()
